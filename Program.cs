73fac123ead8ea6b8475f35a0e7e667bb9ffe2a2
bonamsetty20 <bonamsettyjahnavi@gmail.com>
2023-07-21 13:41:41 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (LargeDataCollection collection = new LargeDataCollection(new object[] { 1, "janu", 3.14 }))
+            {
+                // Adding elements to the collection
+                collection.Add("deva");
+                collection.Add(42);
+
+                // Accessing elements from the collection
+                Console.WriteLine("Element at index 0: " + collection.GetElement(0));
+                Console.WriteLine("Element at index 1: " + collection.GetElement(1));
+
+                // Removing an element from the collection
+                collection.Remove("janu");
+
+                // Accessing the removed element (this will throw an exception)
+                 Console.WriteLine(collection.GetElement(1));
+            }
+        }
+    }
+}
---
+++ object.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment11
+{
+    public class LargeDataCollection : IDisposable
+    {
+        private bool disposed = false;
+        private object[] data;
+
+        public LargeDataCollection(object[] initialData)
+        {
+            data = initialData;
+        }
+        public void Add(object element)
+        {
+        
+            Array.Resize(ref data, data.Length + 1);
+            data[data.Length - 1] = element;
+        }
+        public void Remove(object element)
+        {
+           
+            int index = Array.IndexOf(data, element);
+            if (index >= 0)
+            {
+                for (int i = index; i < data.Length - 1; i++)
+                {
+                    data[i] = data[i + 1];
+                }
+                Array.Resize(ref data, data.Length - 1);
+            }
+        }
+        public object GetElement(int index)
+        {
+      
+            if (index >= 0 && index < data.Length)
+            {
+                return data[index];
+            }
+            throw new IndexOutOfRangeException("Index is out of range.");
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    
+                }
+                data = null;
+                disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        ~LargeDataCollection()
+        {
+            Dispose(false);
+        }
+
+    }
+}
